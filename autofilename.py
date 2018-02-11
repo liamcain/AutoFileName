8ba69ca1d62d58231d2f79c0c750a8955fca6267
evandrocoan <evandrocoan@hotmail.com>
2018-02-11 13:52:43 -0200

    Deprecated the Default.sublime-keymap context afn_insert_dimensions
    by the setting usage.

--- autofilename.py
+++ autofilename.py
@@ -182,9 +182,6 @@ class FileNameComplete(sublime_plugin.EventListener):
                 return
 
     def on_query_context(self, view, key, operator, operand, match_all):
-        if key == "afn_insert_dimensions":
-            return self.get_setting('afn_insert_dimensions',view) == operand
-
         if key == "afn_deleting_slash":  # for reloading autocomplete
             selection = view.sel()[0]
             valid = self.at_path_end(view) and selection.empty() and view.substr(selection.a-1) == FileNameComplete.sep
